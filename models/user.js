796fe042fa0df9ff1a6ec2af7ec5e845fab02903
alecanova <alessandrocanova14@gmail.com>
2020-12-04 21:58:44 +0100

    Add model validation

--- models/user.js
+++ models/user.js
@@ -6,18 +6,71 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {};
 
   User.init({
+
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true
     },
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    emailAddress: DataTypes.STRING,
-    password: DataTypes.STRING
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "A first name is required"
+        },
+        notEmpty: {
+          msg: "Please provide your first name",
+        }, 
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "A last name is required"
+        },
+        notEmpty: {
+          msg: "Please provide your last name",
+        }, 
+      }
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: "The email you entered already exists"
+      },
+      validate: {
+        notNull: {
+          msg: "An email is required"
+        },
+        isEmail: {
+          msg: "Please provide a valid email address"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.VIRTUAL,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "A passsword is required"
+        },
+        notEmpty: {
+          msg: "Please provide a password"
+        },
+        len: {
+          args: [8, 16],
+          msg: 'The password should be between 8 and 16 characters in length'
+        }
+      }
+    },
   }, {
     sequelize,
     modelName: 'User',
+
   });
 
   User.associate = (models) => {
@@ -25,7 +78,7 @@ module.exports = (sequelize, DataTypes) => {
       as: 'owner', //alias
       foreignKey: {
         fieldName: 'userId',
-        allowNull: true,
+        allowNull: false,
       },
     });
   };
